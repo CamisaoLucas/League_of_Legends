a77893e63164bdeb89d79ed2295221ac8d012d1e
lanafe <lanafe99@gmail.com>
2021-06-23 22:28:21 -0300

    Commit Ana

---
+++ LOL.sql
@@ -0,0 +1,4 @@
+select Type, count(Type) as MortesDeDragões from monsters 
+where Type in('EARTH_DRAGON', 'FIRE_DRAGON', 'WATER_DRAGON', 'AIR_DRAGON', 'ELDER_DRAGON', 'DRAGON') 
+group by Type
+order by MortesDeDragões desc; -- Quantos dragões foram feitos?
